57014ce843e005040c840a62a5985b0fd7bc7091
Landon Gravat <railinator4903@gmail.com>
2019-02-11 16:32:58 -0600

    oof

--- app/myrpc/datconfig/DatconfigParser.java
+++ app/myrpc/datconfig/DatconfigParser.java
@@ -1,5 +1,27 @@
 package app.myrpc.datconfig;
 
-public class DatconfigParser {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class DatconfigParser {
+	private boolean comments;
+
+	public DatconfigParser(boolean allowComments) {
+		comments = !!allowComments;
+	}
+
+	public parse(String code) {
+		if (code.matches(Patterns.COMMENT) && !comments)
+	}
+
+	private String[] pareLine(String line) {
+		Matcher valueMatcher = Pattern.compile(Patterns.BODY).matcher(line);
+		String value = valueMatcher.group(0);
+
+	}
+
+	private class Patterns {
+		public static final String BODY = "^[\\w\\d]+(?:\\:[\\w\\d]+)+";
+		public static final String COMMENT = "[ \\t]*(?:\\/\\/.*\\/\\/|\\/.*$)[[ \\t]*";
+	}
 }
